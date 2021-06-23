02f0b223e6e22fa7cbde0906ba8954a2cb8cc674
bornedj <bornedj@miamioh.edu>
2021-06-23 11:09:07 -0400

    added js to make button disappear and to show the stack images

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// adding functionality for the tech stack button
+const displayStack = () => {
+    document.getElementById('stack-button').style.display = 'none';
+    document.getElementById('stack-container').style.display = 'flex';
+}
