27709502ab29d07f9ebf7d4b59f9066b9a81f39d
bornedj <bornedj@miamioh.edu>
2021-06-23 12:00:08 -0400

    the resume buttons and about me buttons are working, adder the nav bar to the resume html

--- src/script.js
+++ src/script.js
@@ -3,3 +3,11 @@ const displayStack = () => {
     document.getElementById('stack-button').style.display = 'none';
     document.getElementById('stack-container').style.display = 'flex';
 }
+
+const openResume = event => {
+    window.location.href="./resume.html";
+}
+
+const openAboutMe = event => {
+    window.location.href="./index.html"
+}
